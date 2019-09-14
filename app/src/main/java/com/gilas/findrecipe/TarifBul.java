026f6314affb54bfd923c6880e97f9cc6cd991de
Emil Mammadov <emilmammadov303@gmail.com>
2019-09-15 00:35:51 +0300

    done for today

--- app/src/main/java/com/gilas/findrecipe/TarifBul.java
+++ app/src/main/java/com/gilas/findrecipe/TarifBul.java
@@ -22,5 +22,40 @@ public class TarifBul extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tarif_bul);
+
+        tabLayout = findViewById(R.id.tablayout);
+        tabHome = findViewById(R.id.tabHome);
+        tabRecipe = findViewById(R.id.tabRecipe);
+        tabFav = findViewById(R.id.tabFav);
+        viewPager = findViewById(R.id.viewPager);
+
+        pageAdapter = new PageAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(pageAdapter);
+
+        tabLayoutListener();
+    }
+
+    private void tabLayoutListener() {
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+
+
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
     }
 }
