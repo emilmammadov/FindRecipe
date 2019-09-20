8e619722a68d4fe8ee5ef50958d5683e0b3a64a2
Emil Mammadov <emilmammadov303@gmail.com>
2019-09-20 12:09:15 +0300

    register mysql is ok.

--- app/src/main/java/com/gilas/findrecipe/Database/DatabaseOperations.java
+++ app/src/main/java/com/gilas/findrecipe/Database/DatabaseOperations.java
@@ -19,9 +19,11 @@ import java.util.Map;
 
 public class DatabaseOperations {
 
+    private static final String TAG = "TAG";
+
     public void login(final Context context, final String username, final String password) {
 
-        String url = "http://192.168.0.11/login.php";
+        String url = "http://192.168.137.1/login.php";
 
         RequestQueue requestQueue = Volley.newRequestQueue(context);
         StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
@@ -38,11 +40,42 @@ public class DatabaseOperations {
             @Override
             public void onErrorResponse(VolleyError error) {
                 Toast.makeText(context, "hata: " + error.toString(), Toast.LENGTH_SHORT).show();
-                Log.e("TAG", "onErrorResponse: " + error.toString() );
+                Log.e("TAG", "onErrorResponse: " + error.toString());
             }
         }) {
             @Override
-            protected Map<String, String> getParams() throws AuthFailureError {
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                params.put("username", username);
+                params.put("password", password);
+
+                return params;
+            }
+        };
+
+        requestQueue.add(stringRequest);
+    }
+
+
+    public void register(final Context context, final String username, final String password) {
+
+        String url = "http://192.168.137.1/register.php";
+
+
+        RequestQueue requestQueue = Volley.newRequestQueue(context);
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.e(TAG, "onResponse: " + response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, "onResponse: " + error);
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() {
                 Map<String, String> params = new HashMap<>();
                 params.put("username", username);
                 params.put("password", password);
