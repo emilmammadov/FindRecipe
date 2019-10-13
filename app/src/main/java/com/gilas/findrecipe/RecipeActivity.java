c4c59d956a1a16361be0634011cdf55e6b15f001
Emil Mammadov <emilmammadov303@gmail.com>
2019-10-13 16:10:54 +0300

    last before DbHelper

--- app/src/main/java/com/gilas/findrecipe/RecipeActivity.java
+++ app/src/main/java/com/gilas/findrecipe/RecipeActivity.java
@@ -6,8 +6,10 @@ import android.widget.CompoundButton;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import com.gilas.findrecipe.data.Recipe;
+import com.gilas.findrecipe.databinding.ActivityRecipeBinding;
 import com.gilas.findrecipe.dboperations.DBHelper;
 import com.gilas.findrecipe.dboperations.DatabaseOperations;
 import com.gilas.findrecipe.fragments.HomeFragment;
@@ -16,36 +18,21 @@ import com.gilas.findrecipe.viewmodels.RecipeViewModel;
 public class RecipeActivity extends AppCompatActivity {
 
     private int id;
-
-    TextView tvTitle, tvIng, tvBody;
-    TextView tvPerson, tvPrep, tvCook;
     CheckBox bookmark;
+    ActivityRecipeBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_recipe);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_recipe);
 
         final Recipe recipeExtra = (Recipe) getIntent().getSerializableExtra(HomeFragment.RECIPE_OBJECT_EXTRA);
+        binding.setRecipe(recipeExtra);
+
         id = recipeExtra.getId();
-        String personText = recipeExtra.getPersonCount() + " " + getResources().getString(R.string.person);
-        String prepText = (recipeExtra.getPrepTimeSec() / 60) + " " + getResources().getString(R.string.minute);
-        String cookText = (recipeExtra.getCookTimeSec() / 60) + " " + getResources().getString(R.string.minute);
 
-        tvPerson = findViewById(R.id.personCountRecipeActivity);
-        tvPrep = findViewById(R.id.prepTimeRecipeActivity);
-        tvCook = findViewById(R.id.cookTimeRecipeActivity);
-        tvTitle = findViewById(R.id.titleRecipeActivity);
-        tvIng = findViewById(R.id.ingredientRecipeActivity);
-        tvBody = findViewById(R.id.bodyRecipeActivity);
-        bookmark = findViewById(R.id.bkmrkCheckbox);
+        bookmark = binding.bkmrkCheckbox;
 
-        tvTitle.setText(recipeExtra.getTitle());
-        tvIng.setText(recipeExtra.getIngredientList());
-        tvBody.setText(recipeExtra.getBody());
-        tvPerson.setText(personText);
-        tvPrep.setText(prepText);
-        tvCook.setText(cookText);
 
         if (new DBHelper(getApplicationContext()).isRecipeExists(id)) {
             bookmark.setChecked(true);
@@ -65,14 +52,11 @@ public class RecipeActivity extends AppCompatActivity {
                     new DatabaseOperations().getFavRecipe(getApplicationContext(), id, new DatabaseOperations.RecipeCallback() {
                         @Override
                         public void onSuccess(Recipe result) {
-                            //new DBHelper(getApplicationContext()).insertRecipeTbl(result);
                             new RecipeViewModel(getApplication()).insertRecipeTbl(result);
                         }
                     });
 
                 } else {
-
-                    //new DBHelper(getApplicationContext()).deleteRecipe(id);
                     new RecipeViewModel(getApplication()).deleteRecipe(id);
 
                 }
